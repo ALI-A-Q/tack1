00b7f2644d35d7fc1b02d5f44151fb7403429cd0
ALI-A-Q <i-k-a4@hotmail.com>
2022-08-21 00:19:44 +0300

    list solv task 1

--- tack 1.py
+++ tack 1.py
@@ -1,3 +1,47 @@
 x=True
-y=int
+y=5
+z=2.5
+s="ahmad"
+print(x,y,z,s)
+y=float(y)
+print(y)
+list1=[1,2,3,4,5]
+tuple1=(10,11,12,13,14,15)
+list1=tuple(list1)
+print(list1)
+print(tuple1)
+dict={1:"ali",2.4:"ahnaa"}
+print(type(dict))
+x=10
+x+=15
+x/=5
+x*=10
+x%=3
+print(11%4)
+print(2**3)
+print(11//3)
+if(10>15):
+    print("true")
+else:
+    print("false")
+x=10
+if(x>15):
+    print(x)
+else:
+    print(x,"smaller 15")
+x=5
+if x<6:print (x)
+##ternary operator
+print ("yes") if x<6 else print("goood")
 
+list2=[2,4,6,8,10]
+if (4 in list2):
+    print("see the numper")
+if(4 and 6 in list2):
+    print("see 4 and 6")
+if(3 or 6 in list2):
+    print("see 3 or 6")
+if (2 and 4 and 5 in list2):
+    print("is see numper")
+else:
+    print("sory not see the numper")
